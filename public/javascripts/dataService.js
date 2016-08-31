56bd84384d6bc8c92a59433c4e1dc958049a3ee9
colblitz <z.joseph.lee.z@gmail.com>
2016-08-31 00:21:05 -0400

    stuff

--- public/javascripts/dataService.js
+++ public/javascripts/dataService.js
@@ -1,10 +1,48 @@
 angular.module('moneystuff')
   .service("dataService", ['$firebaseObject', function($firebaseObject) {
-    var ref = firebase.database().ref();
+    var db = firebase.database();
+    // var ref = firebase.database().ref();
 
-    this.saveNewTransaction = function(amount, description) {
+    //////////////////
+    // Transactions //
+    //////////////////
+
+    this.getAllTransactionsFBObj = function() {
+      return $firebaseObject(db.ref("transactions")
+        .orderByChild("date"));
+    };
+
+    this.getTransactionsFBObj = function(sy, sm, sd, ey, em, ed) {
+      return $firebaseObject(db.ref("transactions")
+        .orderByChild("date")
+        .startAt(processDate(sy, sm, sd))
+        .endAt(processDate(ey, em, ed)));
+    }
+
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    function pad(s) {
+      return (Array(3).join("0") + s.toString()).substring(s.toString().length);
+    };
+
+    function processDate(year, month, day) {
+      if (!year) {
+        year = new Date().getFullYear().toString();
+      }
+      if (!month) {
+        month = (new Date().getMonth() + 1).toString();
+      }
+      if (!day) {
+        day = new Date().getDate().toString();
+      }
+      return year + pad(month) + pad(day);
+    };
+
+    this.saveNewTransaction = function(year, month, day, amount, description) {
       var newTransaction = {
-        date: new Date().getTime(),
+        date: processDate(year, month, day),
         users: {
           joe: true,
           tian: true,
@@ -15,40 +53,47 @@ angular.module('moneystuff')
         amount: amount,
         description: description
       };
-      ref.child("transactions").push(newTransaction);
+      return db.ref("transactions").push(newTransaction);
     };
 
-    this.updateTransaction = function() {
+    this.addRandomTransaction = function() {
+      this.saveNewTransaction("2016", getRandomInt(1, 12).toString(), getRandomInt(1, 31).toString(), getRandomInt(1, 9999), "test");
+    }
 
+    this.deleteTransaction = function(key) {
+      db.ref("transactions/" + key).remove();
+    }
+
+    this.updateTransaction = function(key, transaction) {
+      db.ref("transactions/" + key).set(transaction);
+    };
+
+    this.getTransactionFBObjFromRef = function(ref) {
+      return $firebaseObject(db.ref("transactions/" + ref.key));
     };
 
     this.getRef = function() {
-      return ref.child("test").ref("-KQOlG3rKHt6D6rKg6Qo");
+      // return ref.child("test").ref("-KQOlG3rKHt6D6rKg6Qo");
     };
 
     this.getAllTestsFBObj = function() {
-      return $firebaseObject(ref.child("test"));
+      // return $firebaseObject(ref.child("test"));
     };
-
-
-    this.getAllTransactionsFBObj = function() {
-      return $firebaseObject(ref.child("transactions"));
-    }
 
     this.asdf = function() {
       console.log("lakjsdlfjlajsdf");
-      ref.child("transactions").push({
-        date: new Date().getTime(),
-        users: {
-          joe: true,
-          tian: true,
-        },
-        categories: {
-          food: true
-        },
-        amount: Math.random() * 100,
-        description: "alkjsdf"
-      });
+      // ref.child("transactions").push({
+      //   date: new Date().getTime(),
+      //   users: {
+      //     joe: true,
+      //     tian: true,
+      //   },
+      //   categories: {
+      //     food: true
+      //   },
+      //   amount: Math.random() * 100,
+      //   description: "alkjsdf"
+      // });
     };
 
     return this;
