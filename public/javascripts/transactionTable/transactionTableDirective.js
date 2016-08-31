6689d3c119b957602ee6caed62760e4c20491466
taranir <shikonarella@gmail.com>
2016-08-30 20:56:38 -0400

    add files

---
+++ public/javascripts/transactionTable/transactionTableDirective.js
@@ -0,0 +1,26 @@
+var app = angular.module('moneystuff');
+app.directive('transactionTableDirective', function () {
+  var controller = ['$scope', function ($scope) {
+    function init() {
+      }
+      init();
+      $scope.links = [
+        ['All', '/all'], 
+        ['Tian', '/tianpage'],
+        ['Joe', '/joepage'],
+        ['Summary', '/summary'],
+        ['Settings', '/settings']
+      ]
+      $scope.navigate = function (url) {
+        console.log(url);
+      };
+    }
+  ];
+  return {
+    restrict: 'E', //Default in 1.3+
+    scope: {
+    },
+    controller: controller,
+    templateUrl: 'javascripts/navigation/navTemplate.html'
+  };
+});
